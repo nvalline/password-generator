97fa67ad068e6c041aeddaf8c4299718f5fb889a
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-10 21:26:21 -0600

    added initial conditional for pw length

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
+  function generatePassword() {
+    var pwLength = prompt("Enter the number of characters desired for the password (8 - 128)");
+
+    if (pwLength >= 8 && pwLength <= 128) {
+      return pwLength;
+    } else {
+      pwLength = prompt("Enter the number of characters desired for the password (8 - 128)");
+      return pwLength;
+    }
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  console.log(password);
 }
 
 
