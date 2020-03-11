681c26c5942ceb6e386fdbe509ee0c3d92fd3e4b
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-03-10 22:12:56 -0600

    added question variables and cleaned up charLength

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,29 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+var question1 = "Enter the number of characters desired for the password (8 - 128)";
+var question2 = "Do you want lowercase characters? 'OK' for yes, 'Cancel' for no.";
+var question3 = "Do you want uppercase characters? 'OK' for yes, 'Cancel' for no.";
+var question4 = "Do you want numeric characters? 'OK' for yes, 'Cancel' for no.";
+var question5 = "Do you want special characters? 'OK' for yes, 'Cancel' for no.";
+
 // Write password to the #password input
 function writePassword() {
   function generatePassword() {
-    var pwLength = prompt("Enter the number of characters desired for the password (8 - 128)");
 
-    if (pwLength >= 8 && pwLength <= 128) {
-      return pwLength;
+    // conditional for character length
+    var charLength = prompt(question1);
+
+    if (charLength >= 8 && charLength <= 128) {
+      charLength;
     } else {
-      pwLength = prompt("Enter the number of characters desired for the password (8 - 128)");
-      return pwLength;
+      charLength = prompt(question1);
     }
+
+    // conditional for alphabet lowercase
+    prompt(question2);
+
+    console.log(charLength);
   }
 
   var password = generatePassword();
